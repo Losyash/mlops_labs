12114e174596fbfdd619ff454b442493848108de
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-27 20:59:43 +0500

    Fix code.

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -1,14 +1,5 @@
 #!/bin/env bash
 
-if conda env list | grep -q lab1; then echo "Environment already exists"; else conda create -y -n lab1 python=3.10; fi
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lab1
-
-pip install numpy pandas scikit-learn
-
-cd ~/mlops_labs/lab1
-
 if ! [ -d logs ]; then mkdir logs; fi
 
 echo "Start data_creation.py"
