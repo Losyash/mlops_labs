476fb30e6b96d30c5a42ead6c98ae89597c8c434
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-27 21:21:30 +0500

    Fix pipeline.sh.

--- lab2/pipeline.sh
+++ lab2/pipeline.sh
@@ -3,13 +3,13 @@
 if ! [ -d logs ]; then mkdir logs; fi
 
 echo "Start data_creation.py"
-python data_creation.py
+python lab2/data_creation.py
 
 echo "Start data_preprocessing.py"
-python data_preprocessing.py 
+python lab2/data_preprocessing.py 
 
 echo "Start model_preparation.py"
-python model_preparation.py
+python lab2/model_preparation.py
 
 echo "Start model_preparation.py"
-python model_testing.py
+python lab2/model_testing.py
