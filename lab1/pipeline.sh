8f12c16eb6695f451c0031852564d78ac194d3c3
Alexey Naymushin <alexeynaymushin@urfu.ru>
2023-05-27 13:09:33 +0500

    Fix pipeline.sh.

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -12,13 +12,13 @@ cd ~/mlops_labs/lab1
 if ! [ -d logs ]; then mkdir logs; fi
 
 echo "Start data_creation.py"
-python data_creation.py > logs/lab1.log
+python data_creation.py
 
 echo "Start data_preprocessing.py"
-python data_preprocessing.py > logs/lab1.log
+python data_preprocessing.py 
 
 echo "Start model_preparation.py"
-python model_preparation.py > logs/lab1.log
+python model_preparation.py
 
 echo "Start model_preparation.py"
-python model_testing.py > logs/lab1.log
+python model_testing.py
